938f4b019ba32f3ca09bd41af34790af459e6363
erick <erick.am@hotmail.com>
2023-09-12 11:53:39 -0300

    Adicionado funcionalidade experimental de adição de PDFs pela biblioetaca MultipartFile do Spring

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/PopulacaoController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/PopulacaoController.java
@@ -3,15 +3,18 @@ package br.com.alura.screenmatch.controller;
 import br.com.alura.screenmatch.domain.populacao.OrdenaPorAno;
 import br.com.alura.screenmatch.domain.populacao.Pessoa;
 import br.com.alura.screenmatch.domain.populacao.PopulacaoRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 @Controller
@@ -42,6 +45,23 @@ public class PopulacaoController {
         model.addAttribute("lista", repository.findAll());
         return "populacao/dados";
     }
+
+    @RequestMapping(value = "dados", method = RequestMethod.POST)
+    public String upload(HttpServletRequest request) {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile multipartFile = multipartRequest.getFile("file");
+        File pdf = new File("C:/Users/Fernanda/IdeaProjects/TestesSpring/screenmatch/src/main/resources/file.pdf");
+        try {
+            multipartFile.transferTo(pdf);
+            System.out.println(pdf.getPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+
+            return "redirect:/home";
+        }
+    }
+
 
     @DeleteMapping
     @Transactional //inicia uma transação com o banco de dados com o Spring
